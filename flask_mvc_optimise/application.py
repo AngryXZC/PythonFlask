d0c3877d66a15cc46ace5c4f9d69ae408d873c3a
AngryLeesin <xingzhicheng@imudges.com>
2022-08-04 17:37:00 +0800

    flask基本搭建完成，接下来是主要项目的完成了！

--- flask_mvc_optimise/application.py
+++ flask_mvc_optimise/application.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
-#取环境变量的类
+# 取环境变量的类
 import os
-app=Flask(__name__)
+app = Flask(__name__)
 
-
-manager=Manager(app)
+manager = Manager(app)
 
 app.config.from_pyfile("config/base_setting.py")
-#ops_config=local|production
-#linux export ops_config=local|production
-#windows set ops_config=local|production
+# ops_config=local|production
+# linux export ops_config=local|production
+# windows set ops_config=local|production
 
-#print("环境变量",os.environ)
-app.config['SQLALCHEMY_DATABASE_URI']="mysql://root:123456@127.0.0.1/mysql"
-db=SQLAlchemy(app)
+if "ops_config" in os.environ:
+    app.config.from_pyfile("config/%s_setting.py" % (os.environ['ops_config']))
+else:
+    app.logger.error("环境变量未配置！")
 
+db = SQLAlchemy(app)
